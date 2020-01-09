9e5891592b321224e7c9dfe3f284fe66ea6eace5
leonardorabelo <leonardorabelo@zoho.com>
2020-01-09 20:35:00 -0300

    fixed error in local login

--- config/authConfig.js
+++ config/authConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'googleAuth' : {
-		'clientID' 		: '641023883435-d86l6f99514chq684geuvv5718jtlvil.apps.googleusercontent.com',
-		'clientSecret' 	: 'j1PU7ppG8D8CZJ3bYnwkdfOD',
+		'clientID' 		: 'ID HERE',
+		'clientSecret' 	: 'SECRET HERE',
 		'callbackURL' 	: 'http://localhost:21068/auth/google/callback'
 	}
 };
